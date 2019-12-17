30482d5498e07a622f2f25d2d110518983c84438
Boone B Gorges <boonebgorges@gmail.com>
2019-12-17 14:59:04 -0600

    Connect to groups template, and filter Count query.

--- src/Indexable/Group/QueryIntegration.php
+++ src/Indexable/Group/QueryIntegration.php
@@ -29,9 +29,22 @@ class QueryIntegration {
 		}
 
 		add_filter( 'bp_groups_pre_group_ids_query', [ $this, 'maybe_filter_query' ], 10, 2 );
+		add_filter( 'bp_groups_get_total_groups_sql', [ $this, 'maybe_filter_total_groups_sql' ], 10, 3 );
 
 		// Add header
 //		add_action( 'pre_get_users', array( $this, 'action_pre_get_users' ), 5 );
+	}
+
+	public function cached_results( $formatted_args, $set = null ) {
+		static $results;
+
+		$args_key = md5( json_encode( $formatted_args ) );
+
+		if ( null !== $set ) {
+			$results[ $args_key ] = $set;
+		}
+
+		return isset( $results[ $args_key ] ) ? $results[ $args_key ] : null;
 	}
 
 	public function maybe_filter_query( $results, $r ) {
@@ -43,13 +56,47 @@ class QueryIntegration {
 
 		$formatted_args = $group_indexable->format_args( $r );
 
-		$ep_query = $group_indexable->query_es( $formatted_args, $r );
-
-		var_Dump( $ep_query );
+		$cached = $this->cached_results( $formatted_args );
+		if ( null === $cached ) {
+			$ep_query = $group_indexable->query_es( $formatted_args, $r );
+		} else{
+			$ep_query = $cached;
+		}
 
 		if ( false === $ep_query ) {
 			$r['elasticsearch_success'] = false;
 			return $results;
 		}
+
+		$this->cached_results( $formatted_args, $ep_query );
+
+		$results = array_map(
+			function( $document ) {
+				return $document['ID'];
+			},
+			$ep_query['documents']
+		);
+
+		return $results;
+	}
+
+	public function maybe_filter_total_groups_sql( $total_groups_sql, $sql, $r ) {
+		$group_indexable = Indexables::factory()->get( 'bp-group' );
+
+		if ( ! $group_indexable->elasticpress_enabled( $r ) || apply_filters( 'epbp_skip_group_query_integration', false, $r ) ) {
+			return $results;
+		}
+
+		$formatted_args = $group_indexable->format_args( $r );
+
+		// Should always be cached?
+		$cached = $this->cached_results( $formatted_args );
+
+		$count = 0;
+		if ( isset( $cached['found_documents']['value'] ) ) {
+			$count = $cached['found_documents']['value'];
+		}
+
+		return sprintf( "SELECT %d", intval( $count ) );
 	}
 }
